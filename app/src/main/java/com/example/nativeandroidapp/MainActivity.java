89c80c44f8028bc87f59caa7e3899f85848f20c6
AmrElwkeel <amrgamalelwekeel@gmail.com>
2023-03-17 17:32:54 +0200

    Disable Button

--- app/src/main/java/com/example/nativeandroidapp/MainActivity.java
+++ app/src/main/java/com/example/nativeandroidapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.nativeandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-   public  void login(View v){
-        v.setEnabled(true);
-        System.out.println("Hello");
 
-   }
+    public void login(View v) {
+
+        findViewById(R.id.login).setEnabled(false);
+        ((Button)findViewById(R.id.login)).setText("Disable Button");
+
+//        myView.setEnabled(false);
+//
+//        Button button = (Button) myView;
+//        button.setText("Disable");
+//        v.setEnabled(false);
+//
+//        Button button = (Button) v;
+//        button.setText("Hello");
+
+
+    }
 }
